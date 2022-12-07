5e0a483074a548a83b3c6af7be321e018fa1e1e5
Fontom's <tomtombert7@gmail.com>
2022-12-07 13:47:17 +0100

    Init base commit

---
+++ ExempleClient/Utilisateur.cs
@@ -0,0 +1,25 @@
+﻿namespace ExempleClient
+{
+    public class Utilisateur
+    {
+        public Utilisateur(string pseudo, string nom, string prenom, string password, string email, int isAdmin = 0, int idAvatar = 0)
+        {
+            this.pseudo = pseudo;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.password = password;
+            this.email = email;
+            this.isAdmin = isAdmin;
+            this.idAvatar = idAvatar;
+        }
+
+        public int idUser;
+        public string pseudo;
+        public string nom;
+        public string prenom;
+        public string password;
+        public string email;
+        public int isAdmin;
+        public int idAvatar;
+    }
+}
---
+++ ExempleClient/Utilisateurs.cs
@@ -0,0 +1,17 @@
+﻿namespace ExempleClient
+{
+    public class Utilisateurs
+    {
+        private List<Utilisateur> _utilisateurs;
+
+        public Utilisateurs()
+        {
+            this._utilisateurs = new List<Utilisateur>();
+        }
+
+        public void Add(Utilisateur utilisateur)
+        {
+            this._utilisateurs.Add(utilisateur);
+        }
+    }
+}
